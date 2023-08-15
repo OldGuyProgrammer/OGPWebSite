e552dcc2e693ddcaddbdb6069eef9515647b2c18
Jim Olivi <jim@olivi.us>
2023-08-15 12:28:31 -0400

    Data successfully sent from inout screen though form to server.

--- src/utilities/fetchFromServer.js
+++ src/utilities/fetchFromServer.js
@@ -5,15 +5,13 @@
 // Jim Olivi 2023
 
 export async function postData(data) {
-  console.log("postData");
-  console.log(data);
   try {
     const res = await fetch("http://localhost:3001/savecontact", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: JSON.stringify(data),
+      body: new URLSearchParams(data),
     });
     const result = await res.json();
     console.log("success: ", result);
